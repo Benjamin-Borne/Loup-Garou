9ee3eb93baf6ad44c802f95e1ea4fb960c4f9f19
buggs-bun <mathieu.bunet28@icloud.com>
2024-12-04 17:55:25 +0100

    Add files via upload

--- Role.py
+++ Role.py
@@ -10,27 +10,26 @@ class Joueur:
         role (str) : Rôle du joueur (par défaut, None).
 
     Méthodes :
-        __str__() : Retourne une représentation textuelle du joueur.
         mourir() : Marque le joueur comme mort et met à jour l'interface.
     """
     
-    def __init__(self, nom):
+    def __init__(self):
     
         """
         Initialise un joueur.
-
-        Input:
-            nom (str) : Nom du joueur.
+        Constructeur par default
         """
         
-        self.nom = nom
+        self.nom = ""
         self.est_vivant = True
         self.role = None
+        self.ip=None
 
-    def mourir(self, cycle):
+    def mourir(self):
+        """
+            tue le joueur
+        """
         self.est_vivant = False
-        cycle.chat("Maitre du jeu",f"{self.nom} ({self.role}) est mort.")
-        cycle.afficher_joueurs()
 
 
 # Classes spécifiques pour chaque rôle
@@ -43,18 +42,10 @@ class LoupGarou(Joueur):
         attaquer(joueur) : Attaque un joueur donné.
     """
     
-    def __init__(self, nom):
-        super().__init__(nom)
+    def __init__(self):
+        super().__init__()
         self.role = "Loup-Garou"
-
-    def attaquer(self, joueur, cycle):
-        if joueur:
-            if joueur.est_vivant:
-                joueur.mourir(cycle)
-                cycle.chat("Maitre du jeu",f"{self.nom} (Loup-Garou) attaque {joueur.nom}.")
-        else:
-            cycle.chat("Maitre du jeu",f"{self.nom} (Loup-Garou) n'attaque pas.")
-                
+        
             
 class Voyante(Joueur):
 
@@ -65,17 +56,19 @@ class Voyante(Joueur):
         sonder(joueur) : Découvre le rôle d'un joueur donné.
     """
     
-    def __init__(self, nom):
-        super().__init__(nom)
+    def __init__(self):
+        super().__init__()
         self.role = "Voyante"
 
-    def sonder(self, joueur, cycle):
-        if joueur:
-            cycle.chat("Maitre du jeu",f"{self.nom} (Voyante) sonde {joueur.nom}: {joueur.role}")
-        else:
-            cycle.chat("Maitre du jeu",f"{self.nom} (Voyante) ne sonde pas")
-            
-            
+    def sonder(self):
+        """
+            input : -- 
+                    demande interface pour get cible de voyante !! Victor
+            output : -- role de la cible (str)
+        """
+        cible = "choix de la voyante --> interface" #Victor
+        return cible.role
+  
 class Villageois(Joueur):
 
     """
@@ -83,8 +76,8 @@ class Villageois(Joueur):
     (Aucun pouvoir spécifique, rôle de base.)
     """
     
-    def __init__(self, nom):
-        super().__init__(nom)
+    def __init__(self):
+        super().__init__()
         self.role = "Simple-Villageois"
 
 class Sorciere(Joueur):
@@ -101,49 +94,78 @@ class Sorciere(Joueur):
         tuer(joueur) : Utilise la potion de mort sur un joueur.
     """
     
-    def __init__(self, nom):
-        super().__init__(nom)
+    def __init__(self):
+        super().__init__()
         self.role = "Sorcière"
         self.potion_vie = True
         self.potion_mort = True
 
-    def sauver(self, joueur, cycle):
-        if self.potion_vie:
-            joueur.est_vivant = True
-            cycle.chat("Maitre du jeu",f"{self.nom} (Sorcière) utilise la potion de vie sur {joueur.nom}.")
-            self.potion_vie = False
+    def sauver(self):
+        """
+            input: --
+                    get cible a sauver par interface !! Victor
+            output : maj de l'etat de la cible : vivant (True)
+                     maj de la potion vie (False)
+                     -- cible.nom (str)
+        """
+        cible = "choix de la sorciere --> interface (Joueur)" #Victor
+        self.potion_vie=False
+        cible.est_Vivant=True
+        return cible.nom
+        
 
-    def tuer(self, joueur, cycle):
-        if self.potion_mort:
-            cycle.chat("Maitre du jeu",f"{self.nom} (Sorcière) utilise la potion de mort sur {joueur.nom}.")
-            joueur.mourir(cycle)
-            self.potion_mort = False
+    def tuer(self):
+        """
+            input: -- 
+                    get cible a tuer par interface !! Victor 
+            output : maj de l'etat de la cible : mort (False)
+                     maj de la potion mort (False)
+                     -- cible.nom (str)
+        """
+        cible = "choix de la sorciere --> interface (Joueur)" #Victor
+        self.potion_mort=False
+        cible.mourir()
+        return cible.nom
+        
 
 class Chasseur(Joueur):
 
     """
+    !!! A REVOIR !!!
     Classe représentant un Chasseur.
 
     Méthodes :
         tirer(cible) : Tire sur un joueur après sa mort.
-        mourir() : Redéfinition pour déclencher l'action de tir.
+        mourir() : Redéfinition pour déclencher l'action de tir. 
     """
     
-    def __init__(self, nom):
-        super().__init__(nom)
+    def __init__(self):
+        super().__init__()
         self.role = "Chasseur"
 
-    def tirer(self, cible,cycle):
-        if cible:
-            if self.est_vivant is False:  # Tirer après la mort
-                cycle.chat("Maitre du jeu",f"{self.nom} (Chasseur) tire sur {cible.nom}.")
-                cible.mourir(cycle)
-        else:
-            cycle.chat("Maitre du jeu","Le chasseur n'a pas tiré")
+    def tirer(self,cible):
+        """
+            input: -- cible a tuer (Joueur) 
+            output : maj de l'etat de la cible : mort (False)
+                     -- cible.nom (str)
+        """
+        cible.mourir()
+        return cible.nom
     
-    def mourir(self, cycle):
-        super().mourir(cycle)
-        self.tirer(cycle.trouver_joueur(cycle.interface.action(cycle.playerAlive(),"Chasseur")), cycle)
+    def mourir(self):
+        """
+            !!! A FAIRE !!!
+            input: -- self : instance
+                    get cible a tuer par interface !! Victor
+            output : maj de l'etat du joueur : mort (False)
+                     get cible du chasseur
+                     appel la fonction tirer(cible) (Joueur)
+                     -- 
+                     
+        """
+        super().mourir()
+        cible = "choix du chasseur --> interface (Joueur)" #Victor
+        self.tirer(cible)
 
 class Cupidon(Joueur):
 
@@ -157,21 +179,28 @@ class Cupidon(Joueur):
         lier_amoureux(joueur1, joueur2) : Lie deux joueurs comme amoureux.
     """
     
-    def __init__(self, nom):
-        super().__init__(nom)
+    def __init__(self):
+        super().__init__()
         self.role = "Cupidon"
-        self.amoureux = []  # Liste pour les amoureux
+        self.amoureux = []  # Liste des amoureux
 
-    def lier_amoureux(self, joueur1, joueur2, cycle):
+    def lier_amoureux(self):
+        """
+            input : -- self : instance
+                        get joueur1 et joueur2 a lier par interface !! Victor
+            output : set de self.amoureux avec les deux joueurs (liste)
+                     set des amoureux des deux joueurs
+                     -- tuple des nom des joueurs
+        """
+        joueur1 = "choix1 du cupidon --> interface (Joueur)" #Victor
+        joueur2 = "choix2 du cupidon --> interface (Joueur)" #Victor
 
-        if joueur1 and joueur2:
-            self.amoureux = [joueur1, joueur2]
-            cycle.chat("Maitre du jeu",f"{self.nom} (Cupidon) lie {joueur1.nom} et {joueur2.nom} en tant qu'amoureux.")
-            # Ajoute une référence pour savoir s'ils sont amoureux
-            joueur1.amoureux = joueur2
-            joueur2.amoureux = joueur1
-        else:
-            cycle.chat("Maitre du jeu","Aucun couple n'a été formé")
+        self.amoureux=[joueur1,joueur2]
+        joueur1.amoureux = joueur2
+        joueur2.amoureux = joueur1
+        return (self.amoureux[0].nom,self.amoureux[1].nom)
+
+        
 
 class Voleur(Joueur):
 
@@ -185,25 +214,21 @@ class Voleur(Joueur):
         voler_role(joueur) : Vole le rôle d'un joueur donné.
     """
     
-    def __init__(self, nom):
-        super().__init__(nom)
+    def __init__(self):
+        super().__init__()
         self.role = "Voleur"
-        self.peut_voler = True  # Voleur peut voler une seule fois, durant la première nuit
 
-    def voler_role(self, joueur, cycle):
-        if not self.peut_voler:
-            cycle.chat("Maitre du jeu",f"{self.nom} ne peut plus voler de rôle.")
-            return
-        if joueur == None:
-            cycle.chat("Maitre du jeu","Aucun rôle n'a été volé.")
-            return
+    def voler_role(self):
+        """
+        input : -- self : instance
+                echange des roles
+        output : -- peut etre un return 
+        """
 
-        # Sélectionner aléatoirement un joueur dont le rôle va être volé
-        cycle.chat("Maitre du jeu",f"{self.nom} vole le rôle de {joueur.nom} qui était {joueur.role}.")
-
-        # Échange des rôles
-        ancien_role = joueur.role
-        joueur.role = "Voleur"  # La victime devient un Voleur sans la capacité de voler
-        self.role = ancien_role  # Le voleur prend le rôle de la victime
-        cycle.interface.changeImage(self.role)
-        self.peut_voler = False  # Désactiver la capacité de voler
+        cible = "choix du voleur --> interface (Joueur)" #Victor
+        role_cible=cible.role
+        self.role=role_cible
+        cible.role="Voleur"
+        #changer les images des roles
+        #peut etre un return... surprise!
+        
